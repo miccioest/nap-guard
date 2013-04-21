c25c1ead14fe8c176ec47f3c6e3b0f88e1491a3c
Ariel Lendez <ariellendez@gmail.com>
2013-04-21 19:10:52 -0300

    Modificacion del ConfigActivity.java

--- src/com/diw/snoreguard/ui/ConfigActivity.java
+++ src/com/diw/snoreguard/ui/ConfigActivity.java
@@ -1,20 +1,75 @@
 package com.diw.snoreguard.ui;
 
+import java.util.Calendar;
+
 import com.diw.snoreguard.R;
 import com.diw.snoreguard.provider.AlarmDTO;
 import com.diw.snoreguard.provider.SnoreguardDatabaseHelper;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle; 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class ConfigActivity extends Activity {
-	   
-	    @Override
+	    
+		private static final String TAG = "ConfigActivity";
+        
+		//Variables del timePicker
+		private TextView displayTime;
+		private int pHour;
+	    private int pMinute;
+	    /** This integer will uniquely define the dialog to be used for displaying time picker.*/
+	    static final int TIME_DIALOG_ID = 0;
+
+	    // Métodos usados en el TimePicker -------------------------------------------------------
+	    /** Callback received when the user "picks" a time in the dialog */
+	    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+	        new TimePickerDialog.OnTimeSetListener() {
+	            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+	                pHour = hourOfDay;
+	                pMinute = minute;
+	                updateDisplay();
+	                displayToast();
+	            }
+	        };
+	     
+	    /** Updates the time in the TextView */
+	    private void updateDisplay() {
+	        displayTime.setText(
+	            new StringBuilder()
+	                    .append(pad(pHour)).append(":")
+	                    .append(pad(pMinute)));
+	    }
+	     
+	    /** Displays a notification when the time is updated */
+	    private void displayToast() {
+	        Toast.makeText(this, new StringBuilder().append("Time choosen is ").append(displayTime.getText()),   Toast.LENGTH_SHORT).show();
+	             
+	    }
+	     
+	    /** Add padding to numbers less than ten */
+	    private static String pad(int c) {
+	        if (c >= 10)
+	            return String.valueOf(c);
+	        else
+	            return "0" + String.valueOf(c);
+	    }
+	    // Fin Métodos TimePicker---------------------------------------------------------------
+		
+		@Override
 	    protected void onCreate(Bundle savedInstanceState) { 
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_config);
@@ -91,5 +146,50 @@ public class ConfigActivity extends Activity {
 		public boolean onOptionsItemSelected(MenuItem item) {
 			return MenuSelecciona(item);
 		}
+		
+		protected void onStart() {
+			// TODO Auto-generated method stub
+				super.onStart();
+				
+		        //Hacemos referencia al TimeDetail de la fila Time.
+				displayTime = (TextView) findViewById(R.id.textViewTimeDetail);
+				
+				//Hacemos una referencia a la Fila de la tabla que contiene en Campo 'Time'
+				final TableLayout tl = (TableLayout)findViewById(R.id.tableLayoutConfig);
+				final TableRow trTime = (TableRow)tl.getChildAt(12);
+		        
+				trTime.setClickable(true);
+		        Log.v(TAG, "Voy a la fila 'Time' ");
+		        trTime.setBackgroundColor(Color.TRANSPARENT);
+		        
+		        trTime.setOnClickListener(new View.OnClickListener() {
+		               @Override
+		                public void onClick(View v) {
+		                    // TODO: do your logic here
+		            	   trTime.setBackgroundColor(Color.GRAY);
+		            	   showDialog(TIME_DIALOG_ID);
+		            	   overridePendingTransition(R.anim.fade,R.anim.hold); //Animation
+		                }   
+		        });
+		        
+		        /** Get the current time */
+		        final Calendar cal = Calendar.getInstance();
+		        pHour = cal.get(Calendar.HOUR_OF_DAY);
+		        pMinute = cal.get(Calendar.MINUTE);
+		        
+		        /** Display the current time in the TextView */
+		        updateDisplay();
+						
+		}	
+		        
+		@Override
+	    protected Dialog onCreateDialog(int id) {
+	        switch (id) {
+	        case TIME_DIALOG_ID:
+	            return new TimePickerDialog(this,
+	                    mTimeSetListener, pHour, pMinute, false);
+	        }
+	        return null;
+	    }
 
 }
--- src/com/diw/snoreguard/ui/MainActivity.java
+++ src/com/diw/snoreguard/ui/MainActivity.java
@@ -7,13 +7,18 @@ import com.diw.snoreguard.R;
 import com.diw.snoreguard.provider.AlarmDTO;
 import com.diw.snoreguard.provider.SnoreguardDatabaseHelper;
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View; 
+import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.ToggleButton;
   
 
 public class MainActivity extends Activity {
@@ -27,14 +32,33 @@ public class MainActivity extends Activity {
 		
 	}
 
+	// Method to create each of the items of ActionBar
+    public void CreaMenu(Menu menu) {
+		MenuItem item1 = menu.add(0, 0, 0, "Item 1");
+		{
+			item1.setIcon(R.drawable.device_access_add_alarm);
+			item1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+			
+		}
+		
+		//Activate the item Home to work as an additional item
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+		
+	}
+	
 	@Override
 	protected void onStart() {
 	// TODO Auto-generated method stub
 		super.onStart();
 		
+                //Hacemos una referencia a la tabla
+				final TableLayout tl = (TableLayout)findViewById(R.id.tableLayoutAlarms);
+				//tl.removeAllViews();
 				// Traer la lista de alarmas y 
 				SnoreguardDatabaseHelper db = SnoreguardDatabaseHelper.getInstance(MainActivity.this);
 				ArrayList<AlarmDTO> alarms = db.getAlarms();
+				
 				
 				Iterator<AlarmDTO> it = alarms.iterator();
 				Log.v(TAG, "Recorriendo el array de Alarmas");
@@ -42,27 +66,51 @@ public class MainActivity extends Activity {
 					AlarmDTO alarm = it.next();
 					Log.v(TAG,"id = "+alarm.GetId());
 					
+					//Creamos la nueva fila a insertar en la tabla
+					final TableRow row = new TableRow(this);
+			        int idAlarm = alarm.GetId();
+			        
+					//Creamos el Toogle Button correspondiente a la nueva fila
+					ToggleButton tb = new ToggleButton(this);
+			        tb.setId(idAlarm);
+			        
+					//Colocamos el nombre de la nueva fila
+			        TextView tv = new TextView(this);
+			        tv.setText("Alarma "+idAlarm);
+			        tv.setTextSize(20);
+			        
+			        tv.setId(idAlarm);
+			        
+			        //Armamos la fila y luego la insertamos al final de la tabla
+			        row.addView(tb);
+			        row.addView(tv);
+			        
+			        row.setClickable(true);
+			        row.setBackgroundColor(Color.TRANSPARENT);
+			        
+			        row.setOnClickListener(new View.OnClickListener() {
+			               @Override
+			                public void onClick(View v) {
+			                    // TODO: do your logic here
+			            	   row.setBackgroundColor(Color.GRAY);
+
+			            	   Intent intent = 
+			                           new Intent(MainActivity.this, ConfigActivity.class); 
+			            	   startActivity(intent);
+			            	   overridePendingTransition(R.anim.fade,R.anim.hold); //Animation
+			                }   
+			        });
+			        
+			        tl.addView(row);
+			        
+			        //Insertamos una linea divisoria
+			        
+			        View v = new View(this);
+			        v.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, 4));
+			        v.setBackgroundColor(Color.BLUE);
+ 
+			        tl.addView(v);
 				}
-					
-				
-		        //Obtenemos una referencia al Add Alarm y controlamos si se presiona. 
-
-		        final TableRow firstRow = (TableRow) findViewById(R.id.tableRow1);
-		        firstRow.setClickable(true);
-		        firstRow.setBackgroundColor(Color.TRANSPARENT);
-		        
-		        firstRow.setOnClickListener(new View.OnClickListener() {
-		               @Override
-		                public void onClick(View v) {
-		                    // TODO: do your logic here
-		            	   firstRow.setBackgroundColor(Color.GREEN);
-
-		            	   Intent intent = 
-		                           new Intent(MainActivity.this, ConfigActivity.class); 
-		            	   startActivity(intent);
-		            	   overridePendingTransition(R.anim.fade,R.anim.hold); //Animation
-		                }   
-		        });
 	
 
 	}
@@ -85,10 +133,42 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		super.onCreateOptionsMenu(menu);
+		CreaMenu(menu);
 		return true;
 	}
+	
+	// Method to know which item is clicked
+			public boolean MenuSelecciona(MenuItem item) {
+				switch (item.getItemId()) {
+				
+				case 0:
+					//New Alarm				
+					Intent intent = 
+                    new Intent(MainActivity.this, ConfigActivity.class); 
+					startActivity(intent);
+					overridePendingTransition(R.anim.fade,R.anim.hold); //Animation
+					
+					return true;
+				
+				
+				case android.R.id.home:
+					//Cancel Operation
+					finish();
+		            System.exit(0);
+					
+					return true;	
+				}
+				return false;
+				
+			}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return MenuSelecciona(item);
+	}
+
 	
 
 }
